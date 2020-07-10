acabf8141c024c8eb14e15dd0ba2792ed08b04d8
44546 <68035969+44546@users.noreply.github.com>
2020-07-10 16:40:26 -0700

    added styles.css

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 function rollDice() {
-  console.log("Let's roll the dice!")
+  console.log("Let's roll the dice!");
+
+  var randomNumber=Math.round(Math.random() * 5 + 1);
+  var image = "";
+
+  if (randomNumber == 1){
+    image = "dice 1.png";
+  }
+  if (randomNumber == 2){
+    image = "dice 2.png";
+  }
+  if (randomNumber == 3){
+    image = "dice 3.png";
+  }
+  if (randomNumber == 4){
+    image = "dice 4.png";
+  }
+  if (randomNumber == 5){
+    image = "dice 5.png";
+  }
+  if (randomNumber == 6){
+    image = "dice 6.png";
+  }
+
+  document.querySelector("img").src = image
 }
